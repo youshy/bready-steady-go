26acb6e963e785e08aeafba26dc2c56f1ac94d69
youshy <devilyoushy@gmail.com>
2020-05-26 13:29:09 +0100

    gioui is amazing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module bready-steady-go
+
+go 1.14
+
+require (
+	gioui.org v0.0.0-20200524174833-ad93e3212824
+	github.com/PuerkitoBio/goquery v1.5.1
+)
---
+++ interface.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"image/color"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+)
+
+func loop(w *app.Window) error {
+	gofont.Register()
+	th := material.NewTheme()
+	var ops op.Ops
+	for {
+		e := <-w.Events()
+		switch e := e.(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e.Queue, e.Config, e.Size)
+			l := material.H1(th, "Bready, Steady, GO!")
+			maroon := color.RGBA{127, 0, 0, 255}
+			l.Color = maroon
+			l.Alignment = text.Middle
+			l.Layout(gtx)
+			e.Frame(gtx.Ops)
+		}
+	}
+}
